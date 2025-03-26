4d8fcfcd62afe661fe0e230e533d15f50ec4cc16
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-26 23:46:17 +0100

    Update store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,26 @@
 import { createStore } from 'vuex'
+import Axios from 'axios'
+import { apiUrl } from '@/config'
 
 export default createStore({
   state: {
+    categories: []
   },
   getters: {
+    categoriesAmount (state) {
+      return state.categories.length
+    }
   },
   mutations: {
+    UPDATE_CATEGORIES (state, data) {
+      state.categories = data
+    }
   },
   actions: {
+    async fetchCategories ({ commit }) {
+      const res = await Axios.get(`${apiUrl}/categories`)
+      commit('UPDATE_CATEGORIES', res.data)
+    }
   },
   modules: {
   }
