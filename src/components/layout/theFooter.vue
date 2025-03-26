5a8687c52cf35952e2a98a31d31530b5b3b96563
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-26 23:49:05 +0100

    Add theFooter.vue

---
+++ src/components/layout/theFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer flex justify-content-between p-2 m-4">
+    <p>2020 All rights reserved</p>
+    <p>{{`We have ${categoriesAmount} categories!`}}</p>
+  </footer>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'AppFooter',
+  computed: {
+    ...mapGetters(['categoriesAmount'])
+  }
+}
+</script>
