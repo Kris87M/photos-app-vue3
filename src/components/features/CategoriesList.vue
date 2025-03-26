ae7891e63abb3a49dc506107abe6bc3807df93f9
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-26 23:46:55 +0100

    Add theHeader.vue

---
+++ src/components/features/CategoriesList.vue
@@ -0,0 +1,22 @@
+<template>
+  <ul class="flex gap-2">
+    <Button rounded raised outlined
+    v-for="category in categories"
+    :key="category.id"
+    :label="category.name" />
+  </ul>
+
+</template>
+
+<script>
+import { mapState } from 'vuex'
+import Button from 'primevue/button'
+
+export default {
+  name: 'CategoriesList',
+  components: { Button },
+  computed: {
+    ...mapState(['categories'])
+  }
+}
+</script>
---
+++ src/components/layout/theHeader.vue
@@ -0,0 +1,26 @@
+<template>
+  <header >
+    <div class="header flex justify-content-between flex-wrap m-4">
+      <h3>
+        <router-link
+          to="/"
+          class="no-underline text-color hover:text-blue-700">
+          PhotosApp
+        </router-link> |
+        <span class="text-sm font-normal">share your moments</span>
+      </h3>
+      <Button label="Submit" icon="pi pi-plus" rounded raised/>
+    </div>
+    <categories-list class="m-4 pt-4 border-top-1 border-gray-500"/>
+  </header>
+</template>
+
+<script>
+import Button from 'primevue/button'
+import CategoriesList from '../features/CategoriesList.vue'
+
+export default {
+  name: 'AppHeader',
+  components: { Button, CategoriesList }
+}
+</script>
