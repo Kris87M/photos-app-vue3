7196890c1cfeca1ed9b7db51c76dd21623dd9b22
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-27 17:22:05 +0100

    Add PhotosList component

---
+++ src/components/shared/PhotosList.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex justify-content-around">
+    <p v-if="photos.length === 0" class="alert">
+      ⚠️ Brak zdjęć do wyświetlenia!
+    </p>
+    <photo-summary
+      v-for="photo in photos"
+      :key="photo.id"
+      :title="photo.title"
+      :author="photo.author"
+      :description="photo.description"
+      :src="photo.src"
+      :votes="photo.votes"
+      :category="photo.category"/>
+  </div>
+</template>
+
+<script>
+import PhotoSummary from './PhotoSummary.vue'
+
+export default {
+  name: 'PhotoList',
+  components: { PhotoSummary },
+  props: {
+    photos: {
+      type: Array
+    }
+  }
+}
+</script>
