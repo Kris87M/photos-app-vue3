17e97b1fd2e4adac0d7a9594079c33c05ffb8b85
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-29 23:49:39 +0100

    Add AddPhotPage component

---
+++ src/pages/AddPhotoPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Add Photo</h1>
+  <add-photo-form />
+</template>
+
+<script>
+import AddPhotoForm from '@/components/features/AddPhotoForm.vue'
+
+export default {
+  name: 'AddPhotoPage',
+  components: { AddPhotoForm }
+}
+</script>
