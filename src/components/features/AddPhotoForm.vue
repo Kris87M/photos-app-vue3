9fff0b9a599a63330f5b0a46d055b1f94d84621e
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-30 23:39:13 +0200

    Add validation to form

--- src/components/features/AddPhotoForm.vue
+++ src/components/features/AddPhotoForm.vue
@@ -1,6 +1,6 @@
 <template>
-  <form class="grid" @submit.prevent="handleSubmit">
-    <div class="col">
+  <Form class="formgrid grid" @submit.prevent="handleSubmit" v-slot="{ errors }">
+    <div class="field col col-offset-2">
 
       <!-- msg-success -->
       <Message v-if="isSuccess" severity="success">Success! Your photo has been submitted</Message>
@@ -9,38 +9,46 @@
       <Message v-if="isError" severity="error">Oops… something went wrong…</Message>
 
       <!-- title -->
-      <div class="field">
+      <Field class="field" name="title" v-slot="{ field }" rules="required|min:10|max:60">
         <label class="block">Title</label>
         <InputText
+          v-bind="field"
           type="text"
           v-model="form.title" />
-      </div>
+          <span class="error block text-red-600">{{ errors.title }}</span>
+      </Field>
 
       <!-- author -->
-      <div class="field">
-        <label class="p-d block">Author</label>
+      <Field class="field" name="author" v-slot="{ field }" rules="required|min:3|max:30">
+        <label class="block">Author</label>
         <InputText
+        v-bind="field"
           type="text"
           v-model="form.author" />
-      </div>
+          <span class="error block text-red-600">{{ errors.author }}</span>
+      </Field>
 
       <!-- category -->
-      <div class="field">
+      <Field class="field" name="category" v-slot="{ field }" rules="required">
         <label class="block">Category</label>
         <Listbox
+        v-bind="field"
           v-model="form.category"
           :options="categories"
           optionLabel="name" />
-      </div>
+          <span class="error block text-red-600">{{ errors.category }}</span>
+      </Field>
 
       <!-- description -->
-      <div class="field">
+      <Field class="field" name="description" v-slot="{ field }" rules="required|max:100">
         <label class="block">Description</label>
         <Textarea
+          v-bind="field"
           rows="5"
           cols="30"
           v-model="form.description" />
-      </div>
+          <span class="error block text-red-600">{{ errors.description }}</span>
+      </Field>
 
       <Button
         rounded raised
@@ -50,10 +58,13 @@
         icon="pi pi-plus" />
 
     </div>
-      <div class="col">
-        <image-upload @choose="handleImageChoose"/>
+      <div class="field col">
+        <Field class="p-col" v-slot="{ field }" rules="required|ext:png,jpg" name="image">
+          <span class="error block text-red-600">{{ errors.image }}</span>
+        <image-upload @choose="handleImageChoose" v-bind="field"/>
+        </Field>
       </div>
-  </form>
+  </Form>
 </template>
 
 <script>
@@ -66,10 +77,17 @@ import Message from 'primevue/message'
 import Textarea from 'primevue/textarea'
 import ImageUpload from '../shared/ImageUpload.vue'
 import { mapState } from 'vuex'
+import { Form, Field, defineRule } from 'vee-validate'
+import { required, min, max, ext } from '@vee-validate/rules'
+
+defineRule('required', (value) => required(value) || 'This field is so so required...')
+defineRule('min', (value, params) => min(value, params) || `It should be longer than ${params}`)
+defineRule('max', (value, params) => max(value, params) || `It should be shorter than ${params}`)
+defineRule('ext', (value, params) => ext(value, params) || `You should use one of these extensions: ${params}`)
 
 export default {
   name: 'AddPhotoForm',
-  components: { Button, InputText, ImageUpload, Listbox, Message, Textarea },
+  components: { Button, InputText, ImageUpload, Listbox, Message, Textarea, Form, Field },
   data: () => ({
     form: {
       title: '',
