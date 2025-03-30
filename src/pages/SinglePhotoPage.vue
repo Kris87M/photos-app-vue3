471312522c5c2da698ffb3297453dfc6b932b06a
Krzysztof Mierzejewski <kmierzejewski87@gmail.com>
2025-03-30 22:21:05 +0200

    Add SinglePhoto page

---
+++ src/pages/SinglePhotoPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <single-photo />
+  </div>
+</template>
+
+<script>
+import SinglePhoto from '@/components/features/SinglePhoto.vue'
+
+export default {
+  name: 'SinglePhotoPage',
+  components: { SinglePhoto }
+}
+</script>
